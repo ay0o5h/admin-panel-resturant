09a27d060642fdaa08ef31fae9a7837abea5f1a2
ay0o5h <aayosh553@gmail.com>
2022-03-29 23:28:20 +0300

    add resturant

--- api.js
+++ api.js
@@ -68,3 +68,53 @@ export const ApiOtp = async (info, callback) => {
     })
     .catch((error) => console.log("error", error));
 };
+export const ApiAddResturant = async (info, callback) => {
+  const token = await Cookies.get("token");
+
+  var myHeaders = new Headers();
+  myHeaders.append("token", token);
+  myHeaders.append("Content-Type", "application/json");
+var raw = JSON.stringify(info);
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch(`${URL}/resturant-add`, requestOptions)
+  .then(response => response.json())
+   .then((result) => {
+      if (result.status) return callback(result, null);
+      callback(null, result.errMsg);
+    })
+  .catch(error => console.log('error', error));
+  
+}
+export const ApiFloormap = async (info, callback) => {
+
+
+
+  
+  const token = await Cookies.get("token");
+
+  var myHeaders = new Headers();
+  myHeaders.append("token", token);
+  myHeaders.append("Content-Type", "application/json");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch(`${URL}/resturant/${info}`, requestOptions)
+  .then(response => response.json())
+   .then((result) => {
+      if (result.status) return callback(result, null);
+      callback(null, result.errMsg);
+    })
+  .catch(error => console.log('error', error));
+  
+}
--- pages/sidebar/add-resturant.jsx
+++ pages/sidebar/add-resturant.jsx
@@ -1,8 +1,11 @@
 import { InboxOutlined } from "@ant-design/icons";
-import { Button, Form, Input, message, Steps, TimePicker, Upload } from 'antd';
+import { Button, Form, Input, InputNumber, message, Steps, TimePicker, Upload } from 'antd';
 import moment from 'moment';
+import Image from "next/image";
 import React, { useState } from "react";
+import { ApiAddResturant } from '../../api';
 import RouteProtect from "../../HOC/RouteProtect";
+
 const { Dragger } = Upload;
 const inputStyle = { borderRadius: "5px", padding: "10px", width: "100%" };
 const { Step } = Steps;
@@ -13,16 +16,39 @@ const steps = [
     {
         title: '2',
     },
-    {
-        title: '3',
-    },
 ];
 const AddResturant = () => {
     const [current, setCurrent] = useState(0);
-    const [image, setAddImg] = useState("");
+    const [imageBg, setAddImageBg] = useState("");
+    const [imageFloorMap, setAddimageFloorMap] = useState("");
+    const [data, setData] = useState("")
+
 
     const onFinish = (values) => {
         console.log('Success:', values);
+        const info = {
+            name: values.name,
+            numOfTable: parseInt(values.numOfTable),
+            openDate: values.openDate._d,
+            closeDate: values.closeDate._d,
+            bgImage: imageBg,
+            floorMap: imageFloorMap
+
+        }
+        if (!Object.values(values).includes("undefined")) {
+            ApiAddResturant(info, (datab, error) => {
+                console.log(`info: ${info}`);
+                if (error) return message.error("something went wrong");
+                message.success("Addes Successfuly");
+                let data = datab.rest
+                setData(data)
+                console.log(data)
+                next()
+
+
+            })
+        }
+
     };
 
 
@@ -36,9 +62,45 @@ const AddResturant = () => {
     const onChange = (time, timeString) => {
         console.log(time, timeString);
     }
-    const handleImageUpload = ({ fileList }) => {
+    const handleImageUploadbg = ({ fileList }) => {
         if (fileList[0]) {
-            setAddImg(fileList[0].originFileObj);
+            var formdata = new FormData();
+            formdata.append("image", fileList[0].originFileObj);
+
+            var requestOptions = {
+                method: "POST",
+                body: formdata,
+                redirect: "follow",
+            };
+
+            fetch(`https://api.imgbb.com/1/upload?key=fbb93b107bf8de21163ffa94e7a574d9`, requestOptions)
+                .then((response) => response.json())
+                .then(async (result) => {
+                    const imageBg = result.data.url;
+                    setAddImageBg(imageBg);
+                })
+                .catch((e) => console.log(e));
+        }
+    };
+    const handleImageUploadfloorMap = ({ fileList }) => {
+
+        if (fileList[0]) {
+            var formdata = new FormData();
+            formdata.append("image", fileList[0].originFileObj);
+
+            var requestOptions = {
+                method: "POST",
+                body: formdata,
+                redirect: "follow",
+            };
+
+            fetch(`https://api.imgbb.com/1/upload?key=fbb93b107bf8de21163ffa94e7a574d9`, requestOptions)
+                .then((response) => response.json())
+                .then(async (result) => {
+                    const imageFloorMap = result.data.url;
+                    setAddimageFloorMap(imageFloorMap);
+                })
+                .catch((e) => console.log(e));
         }
     };
     return (
@@ -51,7 +113,7 @@ const AddResturant = () => {
                 </Steps>
                 {current === 0 && (
                     <>
-                        <center> <h3>Enter Basice Information</h3></center>
+                        <center> <h3>Enter Resturant Information</h3></center>
                         <Form
                             name="basic"
                             onFinish={onFinish}
@@ -69,7 +131,7 @@ const AddResturant = () => {
                                 name="numOfTable"
                                 rules={[{ required: true, message: 'Please enter number of Table!' }]}
                             >
-                                <Input style={inputStyle} />
+                                <InputNumber min={1} max={10} defaultValue={3} style={inputStyle} />
                             </Form.Item>
                             <p>open Time</p>
                             <Form.Item
@@ -90,7 +152,7 @@ const AddResturant = () => {
                                 name="bgImage"
                                 rules={[{ required: true, message: 'Please upload resturant image' }]}
                             >
-                                <Dragger onChange={handleImageUpload}>
+                                <Dragger onChange={handleImageUploadbg}>
                                     <p className="ant-upload-drag-icon">
                                         <InboxOutlined />
                                     </p>
@@ -105,7 +167,7 @@ const AddResturant = () => {
                                 name="floorMap"
                                 rules={[{ required: true, message: 'Please upload floor Map' }]}
                             >
-                                <Dragger onChange={handleImageUpload}>
+                                <Dragger onChange={handleImageUploadfloorMap}>
                                     <p className="ant-upload-drag-icon">
                                         <InboxOutlined />
                                     </p>
@@ -119,20 +181,21 @@ const AddResturant = () => {
 
                             <Form.Item>
                                 <Button style={{ borderRadius: "10px", fontWeight: 900, fontSize: "20px", paddingBottom: "20px" }} type="primary" htmlType="submit" block>
-                                    next
+                                    ارسال
                                 </Button>
                             </Form.Item>
                         </Form>
                     </>
                 )}
+                {current === 1 && (
+                    <>
+                        <center> <h3>Add Tables</h3></center>
+                        <Image src={data.bgImage} width="100%" height="100%" />
+                    </>)}
 
                 {/* {current === 0 && <p>test</p>} */}
                 <div className="steps-action">
-                    {current < steps.length - 1 && (
-                        <Button type="primary" onClick={() => next()}>
-                            Next
-                        </Button>
-                    )}
+
                     {current === steps.length - 1 && (
                         <Button type="primary" onClick={() => message.success('Processing complete!')}>
                             Done
